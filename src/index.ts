80fe24d5a0a5bfed81b9267465bd7261cfc7ce15
Zxilly <zhouxinyu1001@gmail.com>
2021-05-15 17:18:19 +0800

    feat: add switch

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-import {RecipeBuilder} from "@blitzjs/installer"
+import { RecipeBuilder } from "@blitzjs/installer"
+import { join } from "path"
 
 const receipt = RecipeBuilder()
   .setName("blitz-authz")
-  .setDescription(
-    "A RBAC & ABAC hybrid authorization middleware for Blitz.js",
-  )
+  .setDescription("A RBAC & ABAC hybrid authorization middleware for Blitz.js")
   .setOwner("Zxilly <zhouxinyu1001@gmail.com>")
   .setRepoLink("https://github.com/node-casbin/blitz-authz")
   .addAddDependenciesStep({
     stepId: "addDeps",
     stepName: "Add dependencies",
     explanation: `Add casbin as a dependency.`,
-    packages: [
-      {name: "casbin", version: "latest"},
-    ],
+    packages: [{ name: "casbin", version: "latest" }],
+  })
+  .addNewFilesStep({
+    stepId: "addApiRoute",
+    stepName: "Add Api Route ",
+    explanation: `We'll setup a query API endpoint to for you to check your rules with useQuery`,
+    targetDirectory: "app/guard/queries/getAbility.ts",
+    templatePath: join(__dirname, "templates", "queries", "getAbility.ts"),
+    templateValues: {},
   })
   .build()
 
