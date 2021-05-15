0e07419ab146fe648b45db19dd19c865e3b46bc8
Zxilly <zhouxinyu1001@gmail.com>
2021-05-15 21:19:59 +0800

    dev: add casbin setup

--- index.ts
+++ index.ts
@@ -13,13 +13,22 @@ const receipt = RecipeBuilder()
     packages: [{ name: "casbin", version: "latest" }],
   })
   .addNewFilesStep({
-    stepId: "addApiRoute",
-    stepName: "Add Api Route ",
-    explanation: `We'll setup a query API endpoint to for you to check your rules with useQuery`,
-    targetDirectory: "app/guard/queries/getAbility.ts",
-    templatePath: join(__dirname, "templates", "queries", "getAbility.ts"),
+    stepId: "addEnforcer",
+    stepName: "Add Enforcer ",
+    explanation: `We'll add a query API endpoint for enforcer.`,
+    targetDirectory: "app/queries/getEnforcer.ts",
+    templatePath: join(__dirname, "templates", "getEnforcer.ts"),
     templateValues: {},
   })
+  .addNewFilesStep({
+    stepId: "addCasbinModelandPolicies",
+    stepName: "Add casbin model and policies files.",
+    explanation: `Next, we will add an example casbin model and files. It shows the basic usage of casbin ACL model.`,
+    targetDirectory: "app/casbin/",
+    templatePath: join(__dirname, "templates", "casbin_files"),
+    templateValues: {},
+  })
+
   .build()
 
 export default receipt
